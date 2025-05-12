81eb10b8d477a8c0fc94c22c38777cbe6e3340da
gutagita <agitamikartikaputeri@gmail.com>
2025-05-12 22:24:59 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,363 @@
+
+
+  <main class="main">
+
+    <!-- Hero Section -->
+    <section id="hero" class="hero section  py-5">
+      <!-- <img src="assets/img/team/beck.jpg" alt="" data-aos="fade-in"> -->
+      <div class="container">
+        <div class="row justify-content-center text-center">
+          <div class="col-lg-10 col-md-12">
+            <h2 class="display-5 fw-bold mb-3" data-aos="fade-up" data-aos-delay="100">Welcome to Our Website</h2>
+            <p   class="lead text-light" data-aos="fade-up" data-aos-delay="200">We are team of talented designers making websites with Bootstrap</p>
+          </div>
+        </div>
+      </div>
+    </section><!-- /Hero Section -->
+
+    
+    <!-- Features Section -->
+    <section id="features" class="features section py-5" >
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Benefit</h2>
+        <p>Why Choose Us?<br></p>
+      </div><!-- End Section Title -->
+      <div class="container">
+         <div class="row gy-4">
+          <div class="col-xl-6 d-flex align-items-center " data-aos="zoom-out" data-aos-delay="100">
+            <img src="assets/img/gambar/kardus.png" class="img-fluid" alt="">
+          </div>
+            <div class="col-xl-6 d-flex p-2 ">
+              <div class="row Top align-self-center gy-5">
+                <div class="col-md-12" data-aos="fade-up" data-aos-delay="400">
+                 <div class="feature-box d-flex align-items-center">
+                    <i class="bi bi-check "></i>
+                    <h3>Aman di kantong</h3>
+                 </div>
+                </div>
+                <div class="col-md-12" data-aos="fade-up" data-aos-delay="200">
+                  <div class="feature-box d-flex align-items-center">
+                    <i class="bi bi-check"></i>
+                    <h3>Aman</h3>
+                </div>
+                </div>
+                <div class="col-md-12" data-aos="fade-up" data-aos-delay="300">
+                  <div class="feature-box d-flex align-items-center">
+                    <i class="bi bi-check"></i>
+                    <h3>Amanah</h3>
+                  </div>
+                </div>
+                <div class="col-md-12" data-aos="fade-up" data-aos-delay="400">
+                  <div class="feature-box d-flex align-items-center">
+                    <i class="bi bi-check"></i>
+                    <h3>Aman di kantong</h3>
+                </div>
+                </div>
+                <!-- End Feature Item -->
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- /Features Section -->
+
+    <!-- Values Section -->
+    <section id="values" class="values section " >
+          <div class="container section-title" data-aos="fade-up">
+            <h2>Our Categories</h2>
+          </div><!-- End Section Title --> 
+
+          <div class="container"> 
+            <div class="row gy-4">
+              <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+                <div class="card">
+                    <img src="assets/img/gambar/book.png" class="img-fluid" alt="">
+                    <!-- <a href="/small.html">Small</a> -->
+                    <h3>Small</h3>
+                </div>
+              </div>
+              <div class="col-lg-4" data-aos="fade-up" data-aos-delay="200">
+                <div class="card">
+                  <img src="assets/img/gambar/medium.png" class="img-fluid" alt="">
+                  <h3>Medium</h3>
+                </div>
+              </div>
+              <div class="col-lg-4" data-aos="fade-up" data-aos-delay="300">
+                <div class="card">
+                  <img src="assets/img/gambar/lemari.png" class="img-fluid" alt="">
+                  <h3>large</h3>
+                </div>
+              </div>
+            </div>
+          </div> 
+    </section><!-- /Values Section -->
+
+ <!-- Tutorial Section -->
+  
+  <section class="container my-5">
+     <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Order</h2>
+        <p>Langkah-Langkah Order<br></p>
+      </div><!-- End Section Title -->
+
+  <div class="wizard d-flex justify-content-between  text-center p-5 bg-white shadow rounded text-center">
+    <div class="step ">
+      <div class="step-circle">1</div>
+      <p class="mt-2">Login</p>
+    </div>
+    <div class="step">
+      <div class="step-circle">2</div>
+      <p class="mt-2">Isi Formulir Order</p>
+    </div>
+    <div class="step">
+      <div class="step-circle">3</div>
+      <p class="mt-2">Submit</p>
+    </div>
+  </div>
+  </section>
+
+    <!-- Testimonials Section -->
+    <section id="testimonials" class="testimonials section my-auto" >
+    <!-- Section Title -->
+    <div class="container section-title" data-aos="fade-up">
+      <h2>Our Customer Love</h2>
+        <!-- <p>What We Do<br></p> -->
+      </div><!-- End Section Title -->
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="swiper init-swiper">
+          <script type="application/json" class="swiper-config">
+            {
+              "loop": true,
+              "speed": 600,
+              "autoplay": {
+                "delay": 5000
+              },
+              "slidesPerView": "auto",
+              "pagination": {
+                "el": ".swiper-pagination",
+                "type": "bullets",
+                "clickable": true
+              },
+              "breakpoints": {
+                "320": {
+                  "slidesPerView": 1,
+                  "spaceBetween": 40
+                },
+                "1200": {
+                  "slidesPerView": 3,
+                  "spaceBetween": 1
+                }
+              }
+            }
+          </script>
+          <div class="swiper-wrapper">
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="stars">
+                  <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                </div>
+                <p>
+                  Amanah sekali, barang sampai dengan selamat dan cepat. Pelayanan juga ramah dan baik. Terima kasih Nitipin.
+                </p>
+                <div class="profile mt-auto">
+                  <img src="assets/img/team/gwan.jpeg" class="testimonial-img" alt="">
+                  <h3>Gwan Sik</h3>
+                  <h4>Mahasiswa</h4>
+                </div>
+              </div>
+            </div><!-- End testimonial item -->
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="stars">
+                  <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                </div>
+                <p>
+                  Aigoo,murah meriah dan cepat. Pelayanan juga ramah dan baik. Terima kasih Nitipin.
+                </p>
+                <div class="profile mt-auto">
+                  <img src="assets/img/team/ae.jpeg" class="testimonial-img" alt="">
+                  <h3>Aesun</h3>
+                  <h4>IRT</h4>
+                </div>
+              </div>
+            </div><!-- End testimonial item -->
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="stars">
+                  <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                </div>
+                <p>
+                  Mudik lebaran saya menitipkan barang ke Nitipin, barang dijaga dengan baik sama dengan kondisi awal. Pelayanan juga ramah dan baik. Terima kasih Nitipin.
+                </p>
+                <div class="profile mt-auto">
+                  <img src="assets/img/team/gm.jpeg" class="testimonial-img" alt="">
+                  <h3>geummyong</h3>
+                  <h4>Mahasiswa Luar Kota</h4>
+                </div>
+              </div>
+            </div><!-- End testimonial item -->
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="stars">
+                  <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                </div>
+                <p>
+                  Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+                </p>
+                <div class="profile mt-auto">
+                  <img src="assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
+                  <h3>Matt Brandon</h3>
+                  <h4>Freelancer</h4>
+                </div>
+              </div>
+            </div><!-- End testimonial item -->
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="stars">
+                  <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                </div>
+                <p>
+                  Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+                </p>
+                <div class="profile mt-auto">
+                  <img src="assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
+                  <h3>John Larson</h3>
+                  <h4>Entrepreneur</h4>
+                </div>
+              </div>
+            </div><!-- End testimonial item -->
+
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+      </div>
+
+    </section><!-- /Testimonials Section -->
+
+    <!-- form review -->
+     <section class="container my-5">
+  <!-- Section Title -->
+  <div class="container section-title" data-aos="fade-up">
+    <h2>Review</h2>
+    <p>Beri kami ulasan Anda<br></p>
+  </div><!-- End Section Title -->
+
+  <div class="p-5 bg-white shadow rounded">
+    <form action="#" method="post">
+      <div class="mb-3">
+        <label for="name" class="form-label">Nama</label>
+        <input type="text" class="form-control" id="name" name="name" required>
+      </div>
+
+  
+
+      <div class="mb-3">
+        <label for="comment" class="form-label">Komentar</label>
+        <textarea class="form-control" id="comment" name="comment" rows="4" required></textarea>
+      </div>
+
+      <button type="submit" class="btn btn-primary">Kirim Ulasan</button>
+    </form>
+  </div>
+</section>
+
+
+  </main><!-- End #main -->
+
+
+  <footer id="footer" class="footer">
+    <div class="container footer-top">
+      <div class="row gy-4">
+        <!-- Bagian About -->
+        <div class="col-lg-4 col-md-6 footer-about">
+          <a href="index.html" class="d-flex align-items-center">
+            <span class="sitename">Nitipin</span>
+          </a>
+          <div class="footer-contact pt-3">
+            <p>A108 Adam Street</p>
+            <p>New York, NY 535022</p>
+            <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
+            <p><strong>Email:</strong> <span>info@example.com</span></p>
+          </div>
+        </div>
+  
+        <!-- Bagian Useful Links -->
+        <div class="col-lg-2 col-md-3 footer-links" >
+          <h4 >Useful Links</h4>
+          <ul>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
+          </ul>
+        </div>
+  
+        <!-- Bagian Our Services -->
+        <div class="col-lg-2 col-md-3 footer-links">
+          <h4>Our Services</h4>
+          <ul>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Web Design</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Web Development</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Marketing</a></li>
+          </ul>
+        </div>
+  
+        <!-- Bagian Follow Us -->
+        <div class="col-lg-4 col-md-12">
+          <h4>Follow Us</h4>
+          <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
+          <div class="social-links d-flex justify-content-center">
+            <a href=""><i class="bi bi-twitter"></i></a>
+            <a href=""><i class="bi bi-facebook"></i></a>
+            <a href=""><i class="bi bi-instagram"></i></a>
+            <a href=""><i class="bi bi-linkedin"></i></a>
+          </div>
+        </div>
+  
+      </div>
+    </div>
+  
+    <!-- Bagian Copyright -->
+    <div class="container copyright text-center mt-4">
+      <p><span></span> <strong class="px-1 sitename">Nitipin</strong></p>
+      <div class="credits">
+        <!-- Credits section, bisa dibiarkan kosong jika tidak diperlukan -->
+      </div>
+    </div>
+  
+  </footer>
+  
+
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
+
+ 
